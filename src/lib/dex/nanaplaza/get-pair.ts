7a2d70f8534c5a3905aa323d121ab32aaba04e3a
Gustav Genberg <gurra.genberg@gmail.com>
2024-11-16 19:31:59 +0700

    fix: nanaplaza sepolia

--- src/lib/dex/nanaplaza/get-pair.ts
+++ src/lib/dex/nanaplaza/get-pair.ts
@@ -1,13 +1,22 @@
 import type { Address, Chain } from "viem";
-import { mantleSepoliaTestnet } from "viem/chains";
+import { mantleSepoliaTestnet, sepolia } from "viem/chains";
 
 export const getPair = (chain: Chain, token: Address) => {
-  switch (chain.name) {
-    case mantleSepoliaTestnet.name: {
+  switch (chain.id) {
+    case mantleSepoliaTestnet.id: {
       switch (token) {
         // pepe
         case "0xAEAFBA08a4B79f08FAeccAE67804cF93FCD08AC1":
           return "0xE36e1E9796C7408C7a9a3BD35e6f32f42a3ca56d";
+      }
+
+      break;
+    }
+    case sepolia.id: {
+      switch (token) {
+        // pepe
+        case "0x05Ad344b2fdE2596EDBA577E21DAfbB55D617c81":
+          return "0xe0958213e1AAE9DDCa831edeF52698e19e5F161a";
       }
 
       break;
--- src/lib/dex/nanaplaza/get-router.ts
+++ src/lib/dex/nanaplaza/get-router.ts
@@ -1,5 +1,5 @@
 import type { Chain } from "viem";
-import { mantleSepoliaTestnet } from "viem/chains";
+import { mantleSepoliaTestnet, sepolia } from "viem/chains";
 
 export const getRouter = (chain: Chain) => {
   switch (chain.id) {
@@ -8,6 +8,12 @@ export const getRouter = (chain: Chain) => {
         address: "0x8FF1CB6116f67f3C8277BA4fe4E879f5bd4Fa55B",
         contract: "minidao_mantle_sepolia_router",
         label: "minidao_mantle_sepolia_router11",
+      } as const;
+    case sepolia.id:
+      return {
+        address: "0x2ca9932919AF8076407Ab45Cd5Ac4fF8E991B0D2",
+        contract: "minidao_sepolia_router",
+        label: "minidao_sepolia_router1",
       } as const;
   }
 
